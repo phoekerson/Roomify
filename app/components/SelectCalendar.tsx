c841fbde346ac36f21d43120880bbc86c268ecce
Caleb Mintoumba <mintoumbacaleb@gmail.com>
2025-03-22 01:25:49 +0100

    deployment

--- app/components/SelectCalendar.tsx
+++ app/components/SelectCalendar.tsx
@@ -1,31 +1,32 @@
 "use client";
 import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
-import {DateRange} from 'react-date-range';
+import {DateRange, Range, RangeKeyDict} from 'react-date-range';
 import { useState } from 'react';
 
 
-export function SelectCalendar(){
-    const [state, setState] = useState([
+export function SelectCalendar() {
+    const [state, setState] = useState<Range[]>([
         {
             startDate: new Date(),
             endDate: new Date(),
             key: "selection",
         },
-    ])
+    ]);
+
     return (
         <>
-        <input type="hidden" name="startDate" value={state[0].startDate.toISOString()}/>
-        <input type="hidden" name="endDate" value={state[0].endDate.toISOString()}/>
-        <DateRange
-            date = {new Date()}
-            showDateDisplay={false}
-            rangeColors={["#FF5A5F"]}
-            ranges={state} 
-            onChange={(item) => setState([item.selection] as any)}
-            minDate={new Date()}
-            direction="vertical"
-        />
+            <input type="hidden" name="startDate" value={state[0].startDate?.toISOString()} />
+            <input type="hidden" name="endDate" value={state[0].endDate?.toISOString()} />
+            <DateRange
+                date={new Date()}
+                showDateDisplay={false}
+                rangeColors={["#FF5A5F"]}
+                ranges={state}
+                onChange={(item: RangeKeyDict) => setState([item.selection])}
+                minDate={new Date()}
+                direction="vertical"
+            />
         </>
-    )
+    );
 }
